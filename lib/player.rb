a3240af7483c7365e7aff39d5c5b0a1d659a72e6
Alex <alex@roadsign.fr>
2019-07-23 18:03:30 +0200

    last commits

--- lib/player.rb
+++ lib/player.rb
@@ -6,38 +6,62 @@ class Player
 	attr_accessor :life_points
 
 # The user enters the name of the players
-def initialize(name_player)
-    @name = name_player
-    @life_points = life_points
-    # Initializing life points to 10
-    @life_points = 10
-end
+	def initialize(name_player)
+    	@name = name_player
+    	@life_points = life_points
+    	# Initializing life points to 10
+    	@life_points = 10
+	end
 
- # Shows player's life
- def show_state
-  	puts "#{name} a #{@life_points} points de vie"
- end
+ 	# Shows player's life
+ 	def show_state
+  		puts "#{name} a #{@life_points} points de vie"
+ 	end
 
-# Inflicts damages to a player
- def gets_damage(damage_got)
-  	@life_points -= damage_got
-  	if @life_points <= 0
+	# Inflicts damages to a player
+ 	def gets_damage(damage_got)
+  		@life_points -= damage_got
+  		if @life_points <= 0
   		puts "Le joueur #{name} a été tué!"
   	end
- end
+ 	end
 
-# The user chooses to attack a player
-def attacks(player_attacked)
-  	puts "Le joueur #{self.name} attaque le joueur #{player_attacked.name}"
-  	damage_got = compute_damage
-  	puts "Il lui inflige #{damage_got} points de dommages"
-  	player_attacked.gets_damage(damage_got)
+	# The user chooses to attack a player
+	def attacks(player_attacked)
+  		puts "Le joueur #{self.name} attaque le joueur #{player_attacked.name}"
+  		damage_got = compute_damage
+  		puts "Il lui inflige #{damage_got} points de dommages"
+  		player_attacked.gets_damage(damage_got)
+	end
+
+	# Computes a random number of damage
+	def compute_damage
+		return rand(1..6)
+	end
+
 end
 
-# Computes a random number of damage
-def compute_damage
-	return rand(1..6)
+class HumanPlayer < Player
+	#giving the accesssors
+	attr_accessor :name, :life_points, :weapon_level
+
+	#the initialize function
+	def initialize(name_player)
+		@life_points = 100
+		@weapon_level = 1
+	end
+
+	# Shows player's life
+ 	def show_state
+  		puts "#{name} a #{@life_points} points de vie & une arme de niveau #{weapon_level}"
+ 	end
+
+ 	def compute_damage
+    rand(1..6) * @weapon_level
+  end
+  
 end
 
-end
+
+
 binding.pry
